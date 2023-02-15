2b2f4330e8364938a6ff2798f8e1355003b5f08a
OmarGal <omargalindol.01@gmail.com>
2023-02-14 18:21:44 -0600

    isr finished

---
+++ isr.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    double salario, porcentaje, sueldo_bruto, sueldo_neto;
+    int dias_trabajados;
+
+    cout << "============ Bienvenido ============" << endl;
+    cout << "Ingresa tu salario: $";
+    cin >> salario;
+    cout << "Ingresa dias trabajados: ";
+    cin >> dias_trabajados;
+
+    sueldo_bruto = salario * dias_trabajados;
+
+    if(sueldo_bruto <= 8954.49){
+        porcentaje = 1.92;
+    }
+    else if(sueldo_bruto <= 75984.55){
+        porcentaje = 6.40;
+    }
+    else if(sueldo_bruto <= 133536.07){
+        porcentaje = 10.88;
+    }
+    else if(sueldo_bruto <= 155229.80){
+        porcentaje = 16;
+    }
+    else if(sueldo_bruto <= 185852.57){
+        porcentaje = 17.92;
+    }
+    else if(sueldo_bruto <= 374837.88){
+        porcentaje = 21.36;
+    }
+    else if(sueldo_bruto <= 590795.99){
+        porcentaje = 23.52;
+    }
+    else if(sueldo_bruto <= 1127926.84){
+        porcentaje = 30;
+    }
+    else if(sueldo_bruto <= 1503902.46){
+        porcentaje = 32;
+    }
+    else if(sueldo_bruto <= 4511707.38){
+        porcentaje = 34;
+    }
+    else{
+        porcentaje = 35;
+    }
+
+    sueldo_neto = sueldo_bruto - (sueldo_bruto * (porcentaje/100));
+
+    cout << "\nSueldo neto despues de un impuesto del " << porcentaje << "% es de: $" << sueldo_neto << endl;
+
+    return 0;
+}
