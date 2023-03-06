16ba049d1b5f39286fefe13f7c6beaaf51b4e7a7
OmarGal <omargalindol.01@gmail.com>
2023-03-06 16:25:05 -0600

    carro.cpp 100

--- carro.cpp
+++ carro.cpp
@@ -3,33 +3,50 @@ using namespace std;
 
 typedef struct carro{
     string marca;
+    string tipo;
     int modelo;
     int precio;
 }Autos;
 
 int main(){
-    int cantidad = 5, opcion;
-    
+    string x;
+    int cantidad = 5, opcion, buscar_modelo;
     Autos Unidades[cantidad];
 
     do{
+        system("clear");
         cout << "1.Guardar\n2.Mostrar\n3.Salir\nOpcion: ";
         cin >> opcion;
+        cout << endl;
         if(opcion == 1){
             for (int i = 0; i < cantidad; i++){
-                cout << "\nIntroduce la marca: ";
-                // getline(cin, Unidades[i].marca); // noup :[
+                cout << "Introduce la marca: ";
                 cin >> Unidades[i].marca;
+                cout << "Introduce el tipo: ";
+                cin >> Unidades[i].tipo;
                 cout << "Introduce el modelo: ";
                 cin >> Unidades[i].modelo;
                 cout << "Introduce el precio: $";
                 cin >> Unidades[i].precio;
+                cout << endl;
             }
         }
-        else{
-
+        else if(opcion == 3){
+            cout << "Finalizado." << endl;
+            break;
         }
-    } while (opcion >= 3);
+        else{
+            for(int i = 0; i < cantidad; i++){
+                cout << i+1 << "." << Unidades[i].marca << endl;
+            }
+            cout << "Selecciona modelo: ";
+            cin >> buscar_modelo;
+            buscar_modelo = buscar_modelo - 1;
+            cout << "\n" << Unidades[buscar_modelo].marca << Unidades[buscar_modelo].tipo << ", " << Unidades[buscar_modelo].modelo << ". $" << Unidades[buscar_modelo].precio << endl;
+            cout << "Presiona una tecla para continuar...";
+            cin >> x;
+        }
+    } while (opcion != 3);
     
 
     return 0;
