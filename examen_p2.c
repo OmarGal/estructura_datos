767070b020401945803d6d45e5b53b5f4f4792a1
OmarGal <omargalindol.01@gmail.com>
2023-03-27 16:44:19 -0600

    lsjf

--- examen_p2.c
+++ examen_p2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-inicializar_cuartos();
+void inicializar_cuartos();
 
 typedef struct Departamentos
 {
@@ -14,11 +14,17 @@ Departamentos Cuartos[6];
 
 
 int main(){
+    inicializar_cuartos();
+
+    for(int i = 0; i < sizeof(Cuartos)/sizeof(Cuartos[0]); i++){
+        printf("\n%d", Cuartos[i].num_cuarto);
+    }
+
 
     return 0;
 }
 
-inicializar_cuartos(){
+void inicializar_cuartos(){
     Cuartos[0].num_cuarto = 101;
     Cuartos[0].num_camas = 2;
     Cuartos[0].piso = 1;
@@ -34,10 +40,20 @@ inicializar_cuartos(){
     Cuartos[2].piso = 2;
     Cuartos[2].precio = 800;
 
-    Cuartos[2].num_cuarto = 201;
-    Cuartos[2].num_camas = 3;
-    Cuartos[2].piso = 2;
-    Cuartos[2].precio = 800;
+    Cuartos[3].num_cuarto = 301;
+    Cuartos[3].num_camas = 2;
+    Cuartos[3].piso = 3;
+    Cuartos[3].precio = 600;
+
+    Cuartos[4].num_cuarto = 401;
+    Cuartos[4].num_camas = 3;
+    Cuartos[4].piso = 4;
+    Cuartos[4].precio = 800;
+
+    Cuartos[5].num_cuarto = 402;
+    Cuartos[5].num_camas = 3;
+    Cuartos[5].piso = 4;
+    Cuartos[5].precio = 800;
 
 
 }
