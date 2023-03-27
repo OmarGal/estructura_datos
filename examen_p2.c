a472c8ade9c80108b7cb06454e2d3d0218250e6d
OmarGal <omargalindol.01@gmail.com>
2023-03-27 16:46:19 -0600

    variable esta_ocupado

--- examen_p2.c
+++ examen_p2.c
@@ -2,12 +2,12 @@
 
 void inicializar_cuartos();
 
-typedef struct Departamentos
-{
+typedef struct Departamentos{
     int num_cuarto;
     int num_camas;
     int piso;
     int precio;
+    int esta_ocupado;
 }Departamentos;
 
 Departamentos Cuartos[6];
@@ -29,31 +29,37 @@ void inicializar_cuartos(){
     Cuartos[0].num_camas = 2;
     Cuartos[0].piso = 1;
     Cuartos[0].precio = 600;
+    Cuartos[0].esta_ocupado = 0;
 
     Cuartos[1].num_cuarto = 102;
     Cuartos[1].num_camas = 2;
     Cuartos[1].piso = 1;
     Cuartos[1].precio = 600;
+    Cuartos[1].esta_ocupado = 0;
 
     Cuartos[2].num_cuarto = 201;
     Cuartos[2].num_camas = 3;
     Cuartos[2].piso = 2;
     Cuartos[2].precio = 800;
+    Cuartos[2].esta_ocupado = 0;
 
     Cuartos[3].num_cuarto = 301;
     Cuartos[3].num_camas = 2;
     Cuartos[3].piso = 3;
     Cuartos[3].precio = 600;
+    Cuartos[3].esta_ocupado = 0;
 
     Cuartos[4].num_cuarto = 401;
     Cuartos[4].num_camas = 3;
     Cuartos[4].piso = 4;
     Cuartos[4].precio = 800;
+    Cuartos[4].esta_ocupado = 0;
 
     Cuartos[5].num_cuarto = 402;
     Cuartos[5].num_camas = 3;
     Cuartos[5].piso = 4;
     Cuartos[5].precio = 800;
+    Cuartos[5].esta_ocupado = 0;
 
 
 }
