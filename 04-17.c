4a9f8549760e58e16c868ab0161b03c53ed8fdf0
OmarGal <omargalindol.01@gmail.com>
2023-04-19 10:08:34 -0600

    tienda terminado

--- 04-17.c
+++ 04-17.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// #include <conio.h>
+#include <conio.h>
 
-void suma_ventas();
 void ventas_dia();
 void mayor_venta_dia();
 void promedio_ventas();
@@ -22,10 +21,10 @@ int main(){
 
     do{
         printf("======= Bienvenido =======\n");
-        printf("1.Ventas por día\n2.Dia con mayor venta\n3.Promedio de venta por dia\n4.Salir\n");
+        printf("1.Ventas por dia\n2.Dia con mayor venta\n3.Promedio de venta por dia\n4.Salir\n");
         printf("opcion: ");
         scanf("%d", &opcion);
-        system("clear");
+        system("cls");
 
         switch (opcion){
         case 1:
@@ -34,15 +33,14 @@ int main(){
         
         case 2:
             mayor_venta_dia();
-            scanf("%d", &opcion);
             break;
         
         case 3:
             promedio_ventas();
-            scanf("%d", &opcion);
             break;
         
         case 4:
+            printf("El programa ha terminado");
             return 0;
             break;
         
@@ -50,7 +48,9 @@ int main(){
             break;
         }
 
-        system("clear");
+        printf("\n\nPresiona cualquier tecla para continuar...");
+        getch();
+        system("cls");
     }while(opcion = 4);
 
 
@@ -58,25 +58,34 @@ int main(){
 }
 
 void ventas_dia(){
-    double venta, total = 0;
+    double venta, total;
+    int mayor = 0, id;
 
-    printf("> Ventas por dia\n");
-    for(int i = 0; i < 7; i++){
-        printf("   - %s\n", dia[i]);
-        for(int j = 0; j < 10; j++){
-            printf("   Venta #%d: $", j+1);
-            scanf("%lf", &tienda[j][i]);
-
+    for (int i = 0; i < 7; i++){
+        total = 0;
+        system("cls");
+        printf("> Ventas por dia");
+        printf("\n -%s\n", dia[i]);
+        for (int j = 0; j < 10; j++){
+            printf("    Venta #%d: $", j+1);
+            scanf("%lf", &venta);
+            total += venta;
+            tienda[j][i] = venta;
+            totales_por_dia[i] = total;
         }
+        if (total > mayor){
+            mayor = total;
+            id = i;
+        }
+        promedios_por_dia[i] = totales_por_dia[i] / 10;
     }
+    id_mayor_venta = id;
 }
 
 void llenar(){
     double venta, total;
     int nMin = 10, nMax = 50;
-    int nRandonNumber;
     int mayor = 0, id;
-    
 
     for(int i = 0; i < 7; i++){
         total = 0;
@@ -96,19 +105,17 @@ void llenar(){
 }
 
 void mayor_venta_dia(){
-    system("clear");
+    system("cls");
     printf("> Dia con mayor ventas\n");
     for(int i = 0; i < 7; i++){
         printf("\n%s: $%.2f", dia[i], totales_por_dia[i]);
     }
     printf("\n\n%s fue el dia con mayor ventas $%.2f", dia[id_mayor_venta], totales_por_dia[id_mayor_venta]);
-
 }
 
 void promedio_ventas(){
-    printf("> Promedio ventas por dia\n");
+    printf("> Promedio de ventas por dia (10 ventas)\n");
     for(int i = 0; i < 7; i++){
         printf("\n%s: $%.2f por cada venta", dia[i], promedios_por_dia[i]);
     }
-
 }
